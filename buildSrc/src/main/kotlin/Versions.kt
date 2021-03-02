baaab95c0d11e33c11cf67d45034413bdbc52853
BillH <billh@loneoaktech.com>
2021-03-01 21:09:41 -0500

    Removed unused "server" project.

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 
 object Versions {
-    const val kotlin = "1.4.30"
+    const val kotlin = "1.4.31"
     const val minSdk = 26
     const val compileSdk = 30
     const val targetSdk = 30
--- server/src/main/kotlin/Main.kt
+++
@@ -1,7 +0,0 @@
-/**
- * Created by BillH on 2/11/2021
- */
-fun main() {
-
-    println("Hello")
-}
